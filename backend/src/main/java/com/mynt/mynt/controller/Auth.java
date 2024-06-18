801de8ebe34d2812f24147f2a945b2e128c80429
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-06-18 20:52:42 +0100

    add sudo code

--- backend/src/main/java/com/mynt/mynt/controller/Auth.java
+++ backend/src/main/java/com/mynt/mynt/controller/Auth.java
@@ -15,7 +15,25 @@ public class Auth {
 
   @GetMapping("")
   public String defalt(){
+
+
+    // recive username && password 
+    // check DB for if user exits 
+    // check for correct password 
+
+    // create function too
+    // 1.create header that has algo and type 
+    // 2.add infomation(claims) into data 
+    // 3.hash steps 1 and 2 to create the secret key  
+
+
     return "happy day!!!";
   }
+
+    // create function too check if JWT is valid
+    // 1a.hash header and payload to create the secret key
+    // 1b. if orginal secret and secret from 1a dont match == has been tampered with
+    // 2. create refresh token
+
 
 }
