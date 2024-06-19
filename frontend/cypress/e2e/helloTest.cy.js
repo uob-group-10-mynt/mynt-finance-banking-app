f4ae08436a26ae13f2d999cbe874265b60136268
alexandermfisher <fisher.alexander.michael@gmail.com>
2024-06-19 10:42:04 +0100

    Revert "test: setup Cypress and first test"
    This reverts commit 50312ceca3010b7361c4e69ddaf31a82547f4f84.

--- frontend/cypress/e2e/helloTest.cy.js
+++
@@ -1,7 +0,0 @@
-describe('hello word page', () => {
-  it('loads', () => {
-    cy.visit('/')
-    cy.get('h1').contains('Hello world!! - body!')
-    cy.get('#root').contains('Hello world1!! - within react')
-  })
-})
