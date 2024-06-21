910e4e1517e979ed5873e083b26cc8af704f5640
koreanMike513 <paulryu1998@gmail.com>
2024-06-21 14:11:40 +0100

    [fix]: Fixed wrong pushed codes from backend
    - unpushed all the code from backend

---
+++ frontend/src/components/CustomButton.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+
+function CustomButton({
+  children,
+  size,
+  colour,
+  variant,
+  ...props
+}) {
+
+  const sizeList = {
+    large: 'lg',
+    medium: 'md',
+    small: 'sm',
+    xsmall: 'xs',
+  }
+
+  const colourList = {
+    gray: 'gray',
+    red: 'red',
+    orange: 'orange',
+    yellow: 'yellow',
+    green: 'green',
+    teal: 'teal',
+    blue: 'blue',
+    cyan: 'cyan',
+    purple: 'purple',
+    pink: 'pink',
+  }
+
+  return (
+    <Button 
+      colorScheme={colourList[colour] || 'teal'}
+      size={sizeList[size] || 'md'}
+      {...props} >{
+      children}
+    </Button>
+  );
+}
+
+
+export default CustomButton;
