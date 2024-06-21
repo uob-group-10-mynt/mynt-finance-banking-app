2a958e89ebbd794a4b5be97ba87db84ba1086998
koreanMike513 <paulryu1998@gmail.com>
2024-06-21 14:55:23 +0100

    [rename]: Renamed app.js to App.js and changed index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app'; // main component
+import App from './App'; 
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+
+const root = ReactDOM.createRoot(document.getElementById('id'));
+
+root.render(<App />);
