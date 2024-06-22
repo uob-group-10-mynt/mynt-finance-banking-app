9e074b34c645431530ff9f9f764567497d98a53c
ZathrasXI <janpatekphillips@gmail.com>
2024-06-22 22:04:13 +0100

    login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6c05a7e (login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields)
 import React, { useState } from "react";
 import { 
   Flex, 
@@ -9,6 +12,7 @@ import {
   Input, 
   Button 
 } from "@chakra-ui/react";
+<<<<<<< HEAD
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -67,14 +71,63 @@ const Login = () => {
 =======
 import React from "react";
 import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+=======
+>>>>>>> 6c05a7e (login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields)
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    //TODO hash password before posting it
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const credentials = { email, password };
+      //TODO send POST request
+      fetch('URL FOR API ENDPOINT', {
+        method: 'POST',
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(credentials)
+      }).then(()=> {
+        console.log('Form submitted', credentials);
+        setEmail('');
+        setPassword('');
+      })
+    }
+
     return(
-        <Flex width="full" align="center" justifyContent="center">
-      <Box p={2}>
-        <Box textAlign="center">
-          <Heading>Login</Heading>
+      <Flex width="full" align="center" justifyContent="center">
+        <Box p={2} className="remittance-page">
+          <Box textAlign="center">
+            <Heading>Login</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <form onSubmit={handleSubmit}>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input 
+                isRequired 
+                type="email" 
+                placeholder="hello@email.com" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}/>
+              </FormControl>
+
+              <FormControl mt={6}>
+                <FormLabel>Password</FormLabel>
+                <Input 
+                  isRequired 
+                  type="password" 
+                  placeholder="*******" 
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+              <Button width="full" mt={4} type="submit">
+                Sign In 
+              </Button>
+            </form>
+          </Box>
         </Box>
+<<<<<<< HEAD
         <Box my={4} textAlign="left">
           <form>
             <FormControl>
@@ -93,6 +146,9 @@ const Login = () => {
       </Box>
     </Flex>
 >>>>>>> e9698c7 (basic routing and login form)
+=======
+      </Flex>
+>>>>>>> 6c05a7e (login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields)
     );
 }
 
