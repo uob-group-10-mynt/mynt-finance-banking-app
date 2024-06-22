a2fbf3aa35da51dc355ab040e453e20e7aff1333
ZathrasXI <janpatekphillips@gmail.com>
2024-06-22 18:49:10 +0100

    basic routing and login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import { 
   Flex, 
@@ -63,6 +64,35 @@ const Login = () => {
           </Box>
         </Box>
       </Flex>
+=======
+import React from "react";
+import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+
+const Login = () => {
+    return(
+        <Flex width="full" align="center" justifyContent="center">
+      <Box p={2}>
+        <Box textAlign="center">
+          <Heading>Login</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="test@test.com" />
+            </FormControl>
+            <FormControl mt={6}>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" placeholder="*******" />
+            </FormControl>
+            <Button width="full" mt={4} type="submit">
+              Sign In
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+>>>>>>> e9698c7 (basic routing and login form)
     );
 }
 
