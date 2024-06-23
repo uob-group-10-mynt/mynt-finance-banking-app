1a90ff8a3cee43ec827438c6f682cb56c974a426
koreanMike513 <paulryu1998@gmail.com>
2024-06-23 22:54:43 +0100

    [feat]: Added useAxios hook
    - useAxios hook receives API then returns its result

---
+++ frontend/src/hooks/useAxios.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
+function useAxios(endpoint, method = "get", options = {}) {
+  const [ fetchData, setFetchData ] = useState(null);
+  const [ error, setError ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+  // const token = localStorage.getItem("accessToken");
+
+  console.log(process.env.REACT_APP_BASE_URL);
+
+  const fetchResult = async () => {
+    try {
+      const response = await axios({
+        url: endpoint,
+        method,
+        baseURL: process.env.REACT_APP_BASE_URL,
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          // Authorization: 
+        },
+        ...options
+      });
+      setFetchData(response.data);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchResult();
+  }, []);
+
+  return { fetchData, error, loading };
+}
+
+export default useAxios;
