e8f0aad8488d0b71be59eb4b2441df598f689178
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-06-25 10:52:30 +0100

    [Revert] :  "used wrong branch sorry - login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields"
    This reverts commit ab59a49ccedc05c2a8029c23a11e53a4063f5618.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,8 @@ import React from "react"
 export default function Home() {
     return(
         <>
-            <h1>Dashboard</h1>
+            <h1>Home page</h1>
+            <div>Dashboard....</div>
         </>
     );
 }
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 6c05a7e (login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields)
 import React, { useState } from "react";
@@ -73,61 +74,22 @@ import React from "react";
 import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 =======
 >>>>>>> 6c05a7e (login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields)
+=======
+import React from "react";
+import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+>>>>>>> 09a0074 ([Revert] :  "used wrong branch sorry - login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields")
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    //TODO hash password before posting it
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const credentials = { email, password };
-      //TODO send POST request
-      fetch('URL FOR API ENDPOINT', {
-        method: 'POST',
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(credentials)
-      }).then(()=> {
-        console.log('Form submitted', credentials);
-        setEmail('');
-        setPassword('');
-      })
-    }
-
     return(
-      <Flex width="full" align="center" justifyContent="center">
-        <Box p={2} className="remittance-page">
-          <Box textAlign="center">
-            <Heading>Login</Heading>
-          </Box>
-          <Box my={4} textAlign="left">
-            <form onSubmit={handleSubmit}>
-              <FormControl>
-                <FormLabel>Email</FormLabel>
-                <Input 
-                isRequired 
-                type="email" 
-                placeholder="hello@email.com" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}/>
-              </FormControl>
-
-              <FormControl mt={6}>
-                <FormLabel>Password</FormLabel>
-                <Input 
-                  isRequired 
-                  type="password" 
-                  placeholder="*******" 
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </FormControl>
-              <Button width="full" mt={4} type="submit">
-                Sign In 
-              </Button>
-            </form>
-          </Box>
+        <Flex width="full" align="center" justifyContent="center">
+      <Box p={2}>
+        <Box textAlign="center">
+          <Heading>Login</Heading>
         </Box>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 09a0074 ([Revert] :  "used wrong branch sorry - login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields")
         <Box my={4} textAlign="left">
           <form>
             <FormControl>
@@ -145,10 +107,13 @@ const Login = () => {
         </Box>
       </Box>
     </Flex>
+<<<<<<< HEAD
 >>>>>>> e9698c7 (basic routing and login form)
 =======
       </Flex>
 >>>>>>> 6c05a7e (login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields)
+=======
+>>>>>>> 09a0074 ([Revert] :  "used wrong branch sorry - login form is setup to send POST request to API; bug fix in remittance form - couldn't input values into form fields")
     );
 }
 
