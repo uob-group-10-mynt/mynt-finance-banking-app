d1fe6ff99a25230340cf8a944f923af9a73c8b86
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-06-25 17:44:13 +0100

    add lazy load && add dashboard page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {createRoot} from 'react-dom';
 import './styles/index.css';
 import {
@@ -8,17 +8,19 @@ import {
     Link,
   } from "react-router-dom";
 
+const App = lazy(() => import('./App'));
+const Error = lazy(() => import('./pages/Error'));
+const CreateAccount = lazy(() => import('./pages/CreateAccount'));
+const SignIn = lazy(() => import('./pages/SignIn'));
+const Remittance = lazy(() => import('./pages/RemittancePage'));
+const Loans = lazy(() => import('./pages/Loans'));
+const Home = lazy(() => import('./pages/Dashboard'));
 
-import App from './App'; 
-import Error from './pages/Error';
-import CreateAccount from './pages/CreateAccount';
-import SignIn from './pages/SignIn';
-import Remittance from './pages/RemittancePage';
-import Loans from './pages/Loans';
 
 const router = createBrowserRouter([
     { path: "*", element: <Error /> },
     { path: "/", element: <App />, errorElement: <Error /> },
+    { path: "home", element: <Home />, errorElement: <Error /> },
     { path: "error", element: <Error />, errorElement: <Error /> },
     { path: "create", element: <CreateAccount />, errorElement: <Error /> },
     { path: "signIn", element: <SignIn />, errorElement: <Error /> },
@@ -28,5 +30,7 @@ const router = createBrowserRouter([
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router} />
+    <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+    </Suspense>
 );
---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,8 @@
+
+function dashboard(){
+    return(
+        <h1>Home && Dashboard</h1>
+    )
+}
+
+export default dashboard;
