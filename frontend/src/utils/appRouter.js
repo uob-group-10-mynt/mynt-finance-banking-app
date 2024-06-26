9340acdc9f4ddfd6f3e1d4b1349b7bf5d2f5d849
Jan Phillips <janpatekphillips@gmail.com>
2024-06-26 17:19:40 +0100

    [refactor]: react router is in separate file, and has been implemented using lazy loading

--- frontend/src/utils/appRouter.js
+++ frontend/src/utils/appRouter.js
@@ -1,26 +1,40 @@
 import { 
+    Suspense, 
+    lazy } from 'react';
+
+import { 
     Route, 
     createBrowserRouter, 
     createRoutesFromElements 
 } from 'react-router-dom';
 
 //Pages
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import RemittancePage from '../pages/RemittancePage'
+const Home = lazy(() => import('../pages/Home'))
+const Login = lazy(() => import('../pages/Login'))
+const RemittancePage = lazy(() => import('../pages/RemittancePage'))
 
-//Layouts
-import RootLayout from '../pages/RootLayout';
+// Layouts
+const RootLayout = lazy(() => import('../pages/RootLayout'))
 
+//Loading components
+const placeholderLoading = <div>loading...</div>
 
 const appRouter = createBrowserRouter(
     createRoutesFromElements(
-        <Route path='/' element={<RootLayout/>}>
-            <Route index element={<Home/>}/>
-            <Route path='login' element={<Login/>}/>
-            <Route path='remittance' element={<RemittancePage/>}/>
+        <Route path='/' element={lazyLoad(RootLayout, placeholderLoading)}>
+            <Route index element={lazyLoad(Home, placeholderLoading)}/>
+            <Route path='login' element={lazyLoad(Login, placeholderLoading)}/>
+            <Route path='remittance' element={lazyLoad(RemittancePage, placeholderLoading)}/>
         </Route>
     )
 );
+
+function lazyLoad(Component, loadingComponent) {
+    return(
+        <Suspense fallback={loadingComponent}>
+            <Component/>
+        </Suspense>
+    )
+}
 
 export default appRouter;
