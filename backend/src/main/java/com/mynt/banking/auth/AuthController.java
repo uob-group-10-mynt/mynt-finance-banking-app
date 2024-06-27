04cff2e00d1c8ccad69338aa2caf95f8847bad64
alexandermfisher <fisher.alexander.michael@gmail.com>
2024-06-27 13:55:57 +0100

    [fix]: merge conflict resolved

--- backend/src/main/java/com/mynt/banking/auth/AuthController.java
+++ backend/src/main/java/com/mynt/banking/auth/AuthController.java
@@ -1,44 +1,44 @@
-package com.mynt.banking.auth;
-
-import com.mynt.banking.service.LoginService;
-import com.mynt.mynt.service.*;
-//import com.mynt.banking.service.DemoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Objects;
-
-@RestController
-//@RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
-public class AuthController {
-
-  private final LoginService loginService;
-
-  @Autowired
-  public AuthController(LoginService loginService){
-    this.loginService = loginService;
-  }
-
-
-  @PostMapping("")
-  public ResponseEntity<Object> defalt(@RequestBody LoginDTO loginDTO){
-    //return 400 if no data is imputed
-    if(loginDTO.getUsername() == null || loginDTO.getPassword() == null){
-      return (ResponseEntity<Object>) ResponseEntity.badRequest().build();
-    }
-
-    //check for token == null
-    JwtDto token = this.loginService.getJwt(loginDTO);
-    String errorMessage = "Error - Invalid username or password";
-    if(Objects.equals(token.getJWT(), errorMessage)){
-      return ResponseEntity.badRequest().body(token);
-    }
-    // else return token
-    return ResponseEntity.ok(token);
-  }
-
-
-}
+//package com.mynt.banking.auth;
+//
+//import com.mynt.banking.service.LoginService;
+//import com.mynt.banking.service.*;
+////import com.mynt.banking.service.DemoService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Objects;
+//
+//@RestController
+////@RequiredArgsConstructor
+//@RequestMapping("/api/v1/auth")
+//public class AuthController {
+//
+//  private final LoginService loginService;
+//
+//  @Autowired
+//  public AuthController(LoginService loginService){
+//    this.loginService = loginService;
+//  }
+//
+//
+//  @PostMapping("")
+//  public ResponseEntity<Object> defalt(@RequestBody LoginDTO loginDTO){
+//    //return 400 if no data is imputed
+//    if(loginDTO.getUsername() == null || loginDTO.getPassword() == null){
+//      return (ResponseEntity<Object>) ResponseEntity.badRequest().build();
+//    }
+//
+//    //check for token == null
+//    JwtDto token = this.loginService.getJwt(loginDTO);
+//    String errorMessage = "Error - Invalid username or password";
+//    if(Objects.equals(token.getJWT(), errorMessage)){
+//      return ResponseEntity.badRequest().body(token);
+//    }
+//    // else return token
+//    return ResponseEntity.ok(token);
+//  }
+//
+//
+//}
--- backend/src/test/java/com/mynt/mynt/util/TestJWT.java
+++ backend/src/test/java/com/mynt/mynt/util/TestJWT.java
@@ -1,64 +1,64 @@
-package com.mynt.mynt.util;
-
-import com.mynt.banking.jwt.JWT;
-import com.nimbusds.jose.JOSEException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.text.ParseException;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-public class TestJWT {
-
-    private JWT jwt;
-
-    @BeforeEach
-    public void setUp() {
-        jwt = new JWT();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        jwt.setExpirationTimeMillis(30000);
-    }
-
-    @Test
-    void testGenerateSecretKey() throws JOSEException {
-        System.out.println("NewKye -> "+JWT.generateSecretKey());
-    }
-
-    @Test
-    void testLoadSecretKey() throws ParseException, JOSEException {
-        JWT key = new JWT();
-        key.loadSecretKey();
-    }
-
-    @Test
-    void testGeneratingJWT() throws InterruptedException, JOSEException, ParseException {
-        String token = jwt.createJWT("james");
-        boolean validToken = jwt.authenticateJWT(token);
-        assertTrue(validToken);
-    }
-
-
-    @Test
-    void testExpiredTime() throws InterruptedException, JOSEException, ParseException {
-        // test for if token is valid within alocated time
-        String token = jwt.createJWT("james");
-        boolean validToken = jwt.authenticateJWT(token);
-        assertTrue(validToken);
-        jwt.setExpirationTimeMillis(1000);
-
-        // test for if token is not valid within alocated time
-        token = jwt.createJWT("james");
-        Thread.sleep(1500);
-        validToken = jwt.authenticateJWT(token);
-        assertFalse(validToken);
-    }
-
-
-}
+//package com.mynt.mynt.util;
+//
+//import com.mynt.banking.jwt.JWT;
+//import com.nimbusds.jose.JOSEException;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import java.text.ParseException;
+//
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//
+//public class TestJWT {
+//
+//    private JWT jwt;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        jwt = new JWT();
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        jwt.setExpirationTimeMillis(30000);
+//    }
+//
+//    @Test
+//    void testGenerateSecretKey() throws JOSEException {
+//        System.out.println("NewKye -> "+JWT.generateSecretKey());
+//    }
+//
+//    @Test
+//    void testLoadSecretKey() throws ParseException, JOSEException {
+//        JWT key = new JWT();
+//        key.loadSecretKey();
+//    }
+//
+//    @Test
+//    void testGeneratingJWT() throws InterruptedException, JOSEException, ParseException {
+//        String token = jwt.createJWT("james");
+//        boolean validToken = jwt.authenticateJWT(token);
+//        assertTrue(validToken);
+//    }
+//
+//
+//    @Test
+//    void testExpiredTime() throws InterruptedException, JOSEException, ParseException {
+//        // test for if token is valid within alocated time
+//        String token = jwt.createJWT("james");
+//        boolean validToken = jwt.authenticateJWT(token);
+//        assertTrue(validToken);
+//        jwt.setExpirationTimeMillis(1000);
+//
+//        // test for if token is not valid within alocated time
+//        token = jwt.createJWT("james");
+//        Thread.sleep(1500);
+//        validToken = jwt.authenticateJWT(token);
+//        assertFalse(validToken);
+//    }
+//
+//
+//}
