5e546766c7775d8561a9178f28a30d7dce7a0fc2
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-06-30 15:22:32 +0100

    [fix] - electron is now working again

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,7 +9,7 @@ dotenv.config();
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'webApp'),
         filename: 'bundle.js'
     },
     module: {
