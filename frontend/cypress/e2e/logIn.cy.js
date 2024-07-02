5959338a2d75cb7c26f58b8ffab0b76398f4d9a4
yy <uv23869@bristol.ac.uk>
2024-07-02 16:38:47 +0800

    [fix] Fix navigation drawer conflict with tests

--- frontend/cypress/e2e/logIn.cy.js
+++ frontend/cypress/e2e/logIn.cy.js
@@ -2,7 +2,6 @@ describe('log in page', () => {
     it('can be found via /login, form accepts valid credentials', () => {
         cy.visit('/login')
 
-        cy.get('[data-cy="LoginLink"]').click()
         cy.get('[data-cy="emailInput"]').type("cypress@test.com")
         cy.get('[data-cy="passwordInput"]').type("password123")
         cy.get('[data-cy="submitButton"]').click()
