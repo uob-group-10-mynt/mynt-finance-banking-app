d6a86f263a28b556b9b06d5429103e23b0a4c290
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-07-04 21:13:57 +0100

    [update]: pad out repositories with methords

--- backend/src/main/java/com/mynt/banking/auth/KycRepository.java
+++ backend/src/main/java/com/mynt/banking/auth/KycRepository.java
@@ -1,10 +1,24 @@
 package com.mynt.banking.auth;
 
 import com.mynt.banking.user.User;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface KycRepository extends JpaRepository<KycEntity,Long> {
 
+    @NotNull
+    @Override
+    List<KycEntity> findAll();
 
+    @NotNull
+    List<KycEntity> findByApplicationId(String applicationId);
 
+    List<KycEntity> findByWorkFlowRunId(String workFlowRunId);
+
+    List<KycEntity> findByStatus(String status);
+
+    List<KycEntity> findByUser(User user);
 }
--- backend/src/main/java/com/mynt/banking/currency_cloud/CurrencyCloudRepository.java
+++ backend/src/main/java/com/mynt/banking/currency_cloud/CurrencyCloudRepository.java
@@ -2,5 +2,12 @@ package com.mynt.banking.currency_cloud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CurrencyCloudRepository extends JpaRepository<CurrencyCloudEntity, String> {
+
+    List<CurrencyCloudEntity> findByUuid(String uuid);
+
+    List<CurrencyCloudEntity> findByUsersId(Long usersId);
+
 }
