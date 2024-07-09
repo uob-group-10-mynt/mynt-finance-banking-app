09970a255ffb11d19b2a66d51f812ebf07eec480
alexandermfisher <fisher.alexander.michael@gmail.com>
2024-07-09 21:04:26 +0100

    [update]: for Me

--- backend/src/main/java/com/mynt/banking/currency_cloud/service/CurrencyCloudAPI.java
+++ backend/src/main/java/com/mynt/banking/currency_cloud/service/CurrencyCloudAPI.java
@@ -14,7 +14,6 @@ public interface CurrencyCloudAPI {
 
     ///////////////////////////////////////////////////////////////////
     ///// AUTHENTICATE API ////////////////////////////////////////////
-
     /** API User Login */
     @PostMapping(value = "/v2/authenticate/api", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     Mono<AuthenticationResponse> authenticate(
@@ -33,31 +32,44 @@ public interface CurrencyCloudAPI {
     ///////////////////////////////////////////////////////////////////
     ///// ACCOUNTS API ////////////////////////////////////////////////
     /** Create Account */
-//    @PostMapping(value = "/v2/accounts/create", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-//    String createAccount(@RequestBody AccountRequest requestBody);
-
     @PostMapping(value = "/v2/accounts/create", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    String createAccount(
+    Mono<JsonNode> createAccount(@RequestBody AccountRequest accountRequest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @PostMapping(value = "/v2/accounts/create", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//    Mono<JsonNode> createAccount(
 //            @RequestHeader("X-Auth-Token") String authToken,
 //            @RequestHeader("User-Agent") String userAgent,
-            @RequestParam("account_name") String accountName,
-            @RequestParam("legal_entity_type") String legalEntityType,
-            @RequestParam("street") String street,
-            @RequestParam("city") String city,
-            @RequestParam("postal_code") String postalCode,
-            @RequestParam("country") String country,
-            @RequestParam(value = "state_or_province", required = false) String stateOrProvince,
-            @RequestParam(value = "brand", required = false) String brand,
-            @RequestParam(value = "your_reference", required = false) String yourReference,
-            @RequestParam(value = "status", required = false) String status,
-            @RequestParam(value = "spread_table", required = false) String spreadTable,
-            @RequestParam(value = "identification_type", required = false) String identificationType,
-            @RequestParam(value = "identification_value", required = false) String identificationValue,
-            @RequestParam(value = "api_trading", required = false) Boolean apiTrading,
-            @RequestParam(value = "online_trading", required = false) Boolean onlineTrading,
-            @RequestParam(value = "phone_trading", required = false) Boolean phoneTrading,
-            @RequestParam(value = "terms_and_conditions_accepted", required = false) Boolean termsAndConditionsAccepted
-    );
+//            @RequestParam("account_name") String accountName,
+//            @RequestParam("legal_entity_type") String legalEntityType,
+//            @RequestParam("street") String street,
+//            @RequestParam("city") String city,
+//            @RequestParam("postal_code") String postalCode,
+//            @RequestParam("country") String country,
+//            @RequestParam(value = "state_or_province", required = false) String stateOrProvince,
+//            @RequestParam(value = "brand", required = false) String brand,
+//            @RequestParam(value = "your_reference", required = false) String yourReference,
+//            @RequestParam(value = "status", required = false) String status,
+//            @RequestParam(value = "spread_table", required = false) String spreadTable,
+//            @RequestParam(value = "identification_type", required = false) String identificationType,
+//            @RequestParam(value = "identification_value", required = false) String identificationValue,
+//            @RequestParam(value = "api_trading", required = false) Boolean apiTrading,
+//            @RequestParam(value = "online_trading", required = false) Boolean onlineTrading,
+//            @RequestParam(value = "phone_trading", required = false) Boolean phoneTrading,
+//            @RequestParam(value = "terms_and_conditions_accepted", required = false) Boolean termsAndConditionsAccepted
+//    );
 
 
 }
