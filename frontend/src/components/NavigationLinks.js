4270394960cb476f06a565bddf9fac59b15e2fb1
Jan Phillips <janpatekphillips@gmail.com>
2024-07-10 18:34:10 +0100

    [refactor]: the links for the navigation bar are a separate component

---
+++ frontend/src/components/NavigationLinks.js
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { LoggedInContext } from "../App";
+
+export default function NavigationLinks() {
+    const [loggedIn, setLoggedIn, logOut] = useContext(LoggedInContext)
+    return (
+        <>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to ='signup' data-cy="SignUpLink">Sign Up</NavLink>
+            <NavLink to='remittance' data-cy="RemittanceLink">Transfer</NavLink>
+            {
+                loggedIn ? 
+                <NavLink to={'login'} data-cy="LogOutLink" onClick={() => {
+                    logOut();
+                }}>Log Out</NavLink>
+                :
+                <NavLink to={'login'} data-cy="LoginLink">Log In</NavLink>
+            }
+        </>
+    )
+}
