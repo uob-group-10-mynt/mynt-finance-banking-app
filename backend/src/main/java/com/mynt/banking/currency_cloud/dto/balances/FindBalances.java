b46a6a492f4f766b4ff71bd867cbb4e1874914c9
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-07-11 20:12:31 +0100

    [update]: add dto

---
+++ backend/src/main/java/com/mynt/banking/currency_cloud/dto/balances/FindBalances.java
@@ -0,0 +1,30 @@
+
+package com.mynt.banking.currency_cloud.dto.balances;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class FindBalances {
+
+    @JsonProperty("currency")
+    @Size(max = 255)
+    @NotNull
+    @Schema(description = "Three-letter ISO currency code.", example = " ")
+    @Builder.Default
+    private String currency = "";
+
+    @JsonProperty("on_behalf_of")
+    @Size(max = 255)
+    @Schema(description = "A contact UUID for the sub-account you're acting on behalf of.", example = " ")
+    @Builder.Default
+    private String amountFrom = "";
+
+}
