f47230c2747c00e0e40b0e0352ff0296c65b7b9f
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-07-15 18:35:11 +0100

    update

--- backend/src/test/java/services/BeneficiariesCCTest.java
+++ backend/src/test/java/services/BeneficiariesCCTest.java
@@ -24,56 +24,56 @@ public class BeneficiariesCCTest {
 
     // NOTE: It will only work on my account as it relies on a beneficiary already in existence
 
-    @Test
-    public void testCreateBeneficiary(){
-
-        // Create Beneficiary:
-        CreateBeneficiaryRequest createBeneficiaryRequest = CreateBeneficiaryRequest.builder()
-                .name("KenyanUser02")
-                .bankAccountHolderName("Kenyan User02")
-                .bankCountry("GB")
-                .currency("KES")
-                .bicSwift("TCCLGB3L")
-                .iban("GB41TCCL12345673185203")
-                .beneficiaryFirstName("Kenyan")
-                .beneficiaryLastName("User")
-                .beneficiaryEntityType("individual")
-                .beneficiaryAddress("Test Address")
-                .beneficiaryCity("Test City")
-                .beneficiaryCountry("KE")
-                .onBehalfOf("fc56ddcc-d3ef-42ed-867f-5759e2a96afe")
-                .build();
-
-        ResponseEntity<JsonNode> createBeneficiaryRequestResult = this.beneficiaryService.create(createBeneficiaryRequest).block();
-
-        assert createBeneficiaryRequestResult != null;
-        int responseCode = createBeneficiaryRequestResult.getStatusCode().value();
-        assertEquals(responseCode, 200);
-
-        JsonNode responseBody = createBeneficiaryRequestResult.getBody();
-        assert responseBody != null;
-        assertEquals(responseBody.get("name").asText(), "KenyanUser02");
-        assertEquals(responseBody.get("bank_account_holder_name").asText(),"Kenyan User02");
-
-
-        // Find Beneficiary - on behalf of Postman: fc56ddcc-d3ef-42ed-867f-5759e2a96afe
-        FindBeneficiaryRequest findBeneficiaryRequest = FindBeneficiaryRequest.builder()
-                .onBehalfOf("fc56ddcc-d3ef-42ed-867f-5759e2a96afe")
-                .build();
-
-        ResponseEntity<JsonNode> findBeneficiaryRequestResult = this.beneficiaryService.find(findBeneficiaryRequest).block();
-
-        assert findBeneficiaryRequestResult != null;
-        int findBeneficiaryRequestResponseCode = createBeneficiaryRequestResult.getStatusCode().value();
-        assertEquals(findBeneficiaryRequestResponseCode, 200);
-
-        JsonNode findBeneficiaryRequestResponseBody = findBeneficiaryRequestResult.getBody();
-        assert findBeneficiaryRequestResponseBody != null;
-        assertEquals(findBeneficiaryRequestResponseBody.get("beneficiaries").get(0).get("name").asText(), "KenyanUser02");
-        assertEquals(findBeneficiaryRequestResponseBody.get("beneficiaries").get(0).get("bank_account_holder_name").asText(),"Kenyan User02");
-
-        // TODO : Delete newly created beneficiary - I am doing throught the dev portal atm:
-        String beneficiaryID = findBeneficiaryRequestResponseBody.get("beneficiaries").get(0).get("id").asText();
-
-    }
+//    @Test
+//    public void testCreateBeneficiary(){
+//
+//        // Create Beneficiary:
+//        CreateBeneficiaryRequest createBeneficiaryRequest = CreateBeneficiaryRequest.builder()
+//                .name("KenyanUser02")
+//                .bankAccountHolderName("Kenyan User02")
+//                .bankCountry("GB")
+//                .currency("KES")
+//                .bicSwift("TCCLGB3L")
+//                .iban("GB41TCCL12345673185203")
+//                .beneficiaryFirstName("Kenyan")
+//                .beneficiaryLastName("User")
+//                .beneficiaryEntityType("individual")
+//                .beneficiaryAddress("Test Address")
+//                .beneficiaryCity("Test City")
+//                .beneficiaryCountry("KE")
+//                .onBehalfOf("fc56ddcc-d3ef-42ed-867f-5759e2a96afe")
+//                .build();
+//
+//        ResponseEntity<JsonNode> createBeneficiaryRequestResult = this.beneficiaryService.create(createBeneficiaryRequest).block();
+//
+//        assert createBeneficiaryRequestResult != null;
+//        int responseCode = createBeneficiaryRequestResult.getStatusCode().value();
+//        assertEquals(responseCode, 200);
+//
+//        JsonNode responseBody = createBeneficiaryRequestResult.getBody();
+//        assert responseBody != null;
+//        assertEquals(responseBody.get("name").asText(), "KenyanUser02");
+//        assertEquals(responseBody.get("bank_account_holder_name").asText(),"Kenyan User02");
+//
+//
+//        // Find Beneficiary - on behalf of Postman: fc56ddcc-d3ef-42ed-867f-5759e2a96afe
+//        FindBeneficiaryRequest findBeneficiaryRequest = FindBeneficiaryRequest.builder()
+//                .onBehalfOf("fc56ddcc-d3ef-42ed-867f-5759e2a96afe")
+//                .build();
+//
+//        ResponseEntity<JsonNode> findBeneficiaryRequestResult = this.beneficiaryService.find(findBeneficiaryRequest).block();
+//
+//        assert findBeneficiaryRequestResult != null;
+//        int findBeneficiaryRequestResponseCode = createBeneficiaryRequestResult.getStatusCode().value();
+//        assertEquals(findBeneficiaryRequestResponseCode, 200);
+//
+//        JsonNode findBeneficiaryRequestResponseBody = findBeneficiaryRequestResult.getBody();
+//        assert findBeneficiaryRequestResponseBody != null;
+//        assertEquals(findBeneficiaryRequestResponseBody.get("beneficiaries").get(0).get("name").asText(), "KenyanUser02");
+//        assertEquals(findBeneficiaryRequestResponseBody.get("beneficiaries").get(0).get("bank_account_holder_name").asText(),"Kenyan User02");
+//
+//        // TODO : Delete newly created beneficiary - I am doing throught the dev portal atm:
+//        String beneficiaryID = findBeneficiaryRequestResponseBody.get("beneficiaries").get(0).get("id").asText();
+//
+//    }
 }
