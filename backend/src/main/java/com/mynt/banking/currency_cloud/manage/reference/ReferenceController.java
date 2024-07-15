553eec8b72901471752a49a1288169cae6930306
LurchK <ruichenglu101306@gmail.com>
2024-07-15 17:02:54 +0100

    [update]: completed currency cloud api of get payer requirements

--- backend/src/main/java/com/mynt/banking/currency_cloud/manage/reference/ReferenceController.java
+++ backend/src/main/java/com/mynt/banking/currency_cloud/manage/reference/ReferenceController.java
@@ -1,12 +1,25 @@
 package com.mynt.banking.currency_cloud.manage.reference;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mynt.banking.currency_cloud.manage.accounts.requests.CreateAccountRequest;
+import com.mynt.banking.currency_cloud.manage.reference.requests.GetPayerRequirementsRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/currency-cloud/reference")
 @RequiredArgsConstructor
 public class ReferenceController {
+    private final ReferenceService referenceService;
+
+    @PostMapping("/getPayerRequirements")
+    public Mono<ResponseEntity<JsonNode>> getPayerRequirements(@RequestBody GetPayerRequirementsRequest request) {
+        return referenceService.getPayerRequirements(request);
+    }
 }
--- backend/src/main/java/com/mynt/banking/currency_cloud/manage/reference/ReferenceService.java
+++ backend/src/main/java/com/mynt/banking/currency_cloud/manage/reference/ReferenceService.java
@@ -1,4 +1,40 @@
 package com.mynt.banking.currency_cloud.manage.reference;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.mynt.banking.currency_cloud.manage.authenticate.AuthenticationService;
+import com.mynt.banking.currency_cloud.manage.balances.requests.FindBalanceAllCurrencies;
+import com.mynt.banking.currency_cloud.manage.reference.requests.GetPayerRequirementsRequest;
+import com.mynt.banking.util.HashMapToQuiryPrams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Service
 public class ReferenceService {
+
+    private final AuthenticationService authenticationService;
+    private final WebClient webClient;
+
+    public Mono<ResponseEntity<JsonNode>> getPayerRequirements(GetPayerRequirementsRequest request) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        HashMap<String, Object> prams = objectMapper.convertValue(request, HashMap.class);
+        String url = "/v2/reference/payer_required_details" + HashMapToQuiryPrams.hashMapToString(prams);
+
+        return webClient
+                .get()
+                .uri(url)
+                .header("X-Auth-Token", authenticationService.getAuthToken())
+                .exchangeToMono(response -> response.toEntity(JsonNode.class))
+                .flatMap(response -> Mono.just(response));
+
+    }
 }
