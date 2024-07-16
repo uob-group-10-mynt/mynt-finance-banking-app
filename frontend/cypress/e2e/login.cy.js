40a181465a46068ad5d394aa19e53820c70186b7
Jan Phillips <janpatekphillips@gmail.com>
2024-07-16 17:24:13 +0100

    [feature]: backend and frontend run in pipeline; login test passes

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -2,7 +2,6 @@ import testUser from '../fixtures/testUser.json';
 
 describe('log in page', () => {
     it('can log a user in and out', () => {
-        cy.createTestUser()
         cy.visit('/')
         cy.get('[data-cy="navButton"]').click()
         cy.get('[data-cy="LoginLink"]').click()
