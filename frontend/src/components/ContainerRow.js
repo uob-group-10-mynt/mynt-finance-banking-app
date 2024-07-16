9670bae912ac773525af010f664b953e985b0916
koreanMike513 <paulryu1998@gmail.com>
2024-07-16 11:30:35 +0100

    [update]: Added row color chaning funcions on mouse enter

--- frontend/src/components/ContainerRow.js
+++ frontend/src/components/ContainerRow.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import { Box } from "@chakra-ui/react";
 
 function ContainerRow({ info }) {
-  console.log(info);
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <Box 
       display='flex'
       flexDirection='row'
       alignItems="center"
       justifyContent="space-around"
+      backgroundColor={isHovered ? '#f0f0f0' : 'transparent'}
+      transition={'background-color 0.3s'}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={info.onClick}
     >
       {info.render(info)}
     </Box>
