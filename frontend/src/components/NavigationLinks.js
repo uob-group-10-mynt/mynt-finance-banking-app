057a76c02296a5d7a27e20433a7f2af72879b6a0
Jan Phillips <janpatekphillips@gmail.com>
2024-07-17 21:14:07 +0100

    [feature]: transfer and log out links only visible when logged in; sign up and log in links on visible when logged out

--- frontend/src/components/NavigationLinks.js
+++ frontend/src/components/NavigationLinks.js
@@ -2,32 +2,37 @@ import {useContext} from "react";
 import {LoggedInContext} from "../App";
 import {Link} from "@chakra-ui/react";
 
-const pages = [
-    {href: "/", text: "Home"},
-    {href: "/remittance", text: "Transfer", testId: "RemittanceLink"},
-    {href: "/signup", text: "Sign Up", testId: "SignUpLink"},
-    // Add more links as needed
-];
 
-const links = pages.map((page, index) => (
-    <Link key={index} href={page.href} mx="2" data-cy={page.testId}>
-        {page.text}
-    </Link>
-));
+function createLinks(dataAboutPages) {
+    return (
+        dataAboutPages.map((page, index) => (
+            <Link key={index} href={page.href} mx="2" data-cy={page.testId} onClick={page.onClick}>
+            {page.text}
+        </Link>  
+        ))
+    )
+}
 
 export default function NavigationLinks() {
     const [loggedIn, setLoggedIn, logOut] = useContext(LoggedInContext)
-    return (
-        <>
-            {links}
-            {
-                loggedIn ?
-                    <Link href={'login'} mx="2" data-cy="LogOutLink" onClick={() => {
-                        logOut();
-                    }}>Log Out</Link>
-                    :
-                    <Link href={'login'} mx="2" data-cy="LoginLink">Log In</Link>
-            }
-        </>
-    )
+    const loggedInData = [
+        {href: "remittance", text: "Transfer", testId: "RemittanceLink"},
+        {href: "login", text: "Log Out", testId: "LogOutLink", onClick: logOut}
+    ]
+    const loggedOutData = [
+        {href: "signup", text: "Sign Up", testId: "SignUpLink"},
+        {href: "login", text: "Log In", testId: "LogInLink"},
+    ]
+    const pagesThatAreAlwaysVisible = [
+        {href: "/", text: "Home"}
+    ];
+
+    let links = createLinks(pagesThatAreAlwaysVisible)
+    if (loggedIn) {
+        links.push(createLinks(loggedInData))
+    } else {
+        links.push(createLinks(loggedOutData))
+    }
+
+    return links;
 }
