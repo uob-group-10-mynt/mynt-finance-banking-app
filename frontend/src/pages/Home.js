c37442b97cd07a6332302714af17e96caa3bad10
koreanMike513 <paulryu1998@gmail.com>
2024-07-19 22:28:31 +0100

    [fix]: Fixed amount display
    - for every 3 digits , will be added
    - for this useFormatAmount hook has been added
    - all pages containing amounts has reflected this change

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
+
+import useFormatAmount from '../hooks/useFormatAmount';
 import CustomButton from "../components/forms/CustomButton";
 import CustomText from "../components/CustomText";
 import Icon from "../components/Icon";
 import Container from "../components/Container";
 import InfoBlock from "../components/InfoBlock";
-import { Box } from "@chakra-ui/react";
-import { useNavigate } from 'react-router-dom';
+
 
 const fetchAccountData = [
     {
@@ -12,7 +15,7 @@ const fetchAccountData = [
         'account_reference': '66f51c98-1ef8-4e48-97de-aac0353ba2b4',
         'bank': 'mynt',
         'label': 'Mynt Dollar Account',
-        'balance': '100',
+        'balance': '1010234.0',
         'currency': 'dollar',
         'currencySymbol': '$'
     },
@@ -21,7 +24,7 @@ const fetchAccountData = [
         'account_reference': '66f51c98-1ef8-4e48-97de-aac0353ba2b4',
         'bank': 'others',
         'label': 'Mynt Pound Account',
-        'balance': '100',
+        'balance': '1000.0',
         'currency': 'pound',
         'currencySymbol': '£'
     },
@@ -33,6 +36,11 @@ export default function Home() {
     const accountKeyFn = (info) => info.id;
     const conversionKeyFn = (info) => info.id;
 
+    const handleSendOnClick = (e) => {
+        e.stopPropagation();
+        console.log("SEND BUTTON CLICKED");
+    }
+
     const accountData = fetchAccountData.map((data) => {
         return {
             ...data,
@@ -42,9 +50,9 @@ export default function Home() {
                         <Icon name={data.bank} />
                         <InfoBlock>
                             <CustomText gray small>{data.label}</CustomText>
-                            <CustomText black big>{data.currencySymbol}{data.balance}</CustomText>
+                            <CustomText black big>{useFormatAmount(data.balance, data.currencySymbol)}</CustomText>
                         </InfoBlock>
-                        <CustomButton side>Send</CustomButton>
+                        <CustomButton side onClick={(e) => handleSendOnClick(e)}>Send</CustomButton>
                         
                     </>
                 );
