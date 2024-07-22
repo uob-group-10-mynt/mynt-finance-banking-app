bd534426a32b007cc6b23a37462c40042d6e210f
alexandermfisher <fisher.alexander.michael@gmail.com>
2024-07-22 16:21:21 +0100

    [feat]: added transfer service

---
+++ backend/src/main/java/com/mynt/banking/currency_cloud/pay/transfers/TransferService.java
@@ -0,0 +1,37 @@
+package com.mynt.banking.currency_cloud.pay.transfers;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mynt.banking.currency_cloud.manage.authenticate.AuthenticationService;
+import com.mynt.banking.currency_cloud.pay.payments.requests.CreatePaymentRequest;
+import com.mynt.banking.currency_cloud.pay.transfers.requests.CreateTransferRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class TransferService {
+
+    private final AuthenticationService authenticationService;
+    private final WebClient webClient;
+
+    public Mono<ResponseEntity<JsonNode>> createTransfer(CreateTransferRequest requestBody) {
+        return webClient
+                .post()
+                .uri("/v2/payments/create")
+                .header("X-Auth-Token", authenticationService.getAuthToken())
+                .bodyValue(requestBody)
+                .exchangeToMono(response -> response.toEntity(JsonNode.class))
+                .flatMap(response -> {
+                    if(response.getStatusCode().is2xxSuccessful()) {
+                        JsonNode jsonNode = response.getBody();
+                        ResponseEntity<JsonNode> newResponseEntity = new ResponseEntity<>(jsonNode, response.getStatusCode());
+                        return Mono.just(newResponseEntity);
+                    }
+                    return Mono.just(response);
+                });
+    }
+}
