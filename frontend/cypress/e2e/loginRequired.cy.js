a65f5db4a241d4db99773c92323aada9d54bf15b
Jan Phillips <janpatekphillips@gmail.com>
2024-07-22 15:34:05 +0100

    [fix]: '/dashboard' is a required route; refactored test

--- frontend/cypress/e2e/loginRequired.cy.js
+++ frontend/cypress/e2e/loginRequired.cy.js
@@ -1,7 +1,11 @@
 describe('user must be logged in to access remittance page', () => {
-    it("redirected to '/login' when not logged in and try to go to '/remittance'", () => {
-        cy.visit('/remittance')
-        cy.url().should('eq', 'http://localhost:9001/login')
+    it("redirected to '/login' when not logged in and try to go to a protected route", () => {
+        let protectedRoutes = ['remittance', 'dashboard']
+        protectedRoutes.forEach((route) => {
+            cy.visit(`/${route}`)
+            cy.url().should('eq', 'http://localhost:9001/login')
+
+        })
     })
 
     it("can access '/remittance' when logged in", () => {
