6f16fc5671b9c8aeaaabffe15520a750f562eeea
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2024-07-22 20:21:55 +0100

    [update]: add errors && add tests

--- backend/src/test/java/services/KycTests.java
+++ backend/src/test/java/services/KycTests.java
@@ -65,10 +65,11 @@ public class KycTests {
                 .password("password")
                 .build();
 
-        SDKResponse response = this.kycService.getOnfidoSDK(signUpRequest);
+        ResponseEntity<SDKResponse> response = this.kycService.getOnfidoSDK(signUpRequest);
 
         assert response != null;
-        assertEquals(response.getStage(),"SDKResponceDTO");
+        assertEquals(response.getStatusCode().value(), 200);
+        assertEquals(response.getBody().getStage(),"SDKResponceDTO");
 
     }
 
@@ -87,10 +88,10 @@ public class KycTests {
                 .password("password")
                 .build();
 
-        SDKResponse response = this.kycService.getOnfidoSDK(signUpRequest);
+        ResponseEntity<SDKResponse> response = this.kycService.getOnfidoSDK(signUpRequest);
 
         assert response != null;
-        assertEquals(response.getStage(),"SDKResponceDTO");
+        assertEquals(response.getBody().getStage(),"SDKResponceDTO");
 
 
         String email = "test"+testAccountNum+"@test.com";
--- frontend/cypress/e2e/signUp.cy.js
+++ frontend/cypress/e2e/signUp.cy.js
@@ -2,7 +2,7 @@ import testUser from '../fixtures/testUser.json';
 
 
 let date = new Date();
-let seconds = date.getUTCSeconds();
+let seconds = date.getTime();
 
 
 
@@ -10,16 +10,15 @@ describe('log in page', () => {
     it('can log a user in and out', () => {
         cy.visit('/signup')
         
-        cy.get('[data-cy="emailInput"]').type("bristoldsfasfdasfasfas@bristol.ac.uk")
-        cy.get('[data-cy="firstNameInput"]').type("firstName")
-        cy.get('[data-cy="surnameInput"]').type("sername")
-        cy.get('[data-cy="DOBInput"]').type("2020-01-01")
-        cy.get('[data-cy="AddressInput"]').type("BS8 1HB")
-        cy.get('[data-cy="PhoneNumberInput"]').type("+44 782479472")
+        cy.get('[data-cy="emailInput"]').type("test"+seconds+"@bristol.ac.uk") 
+        cy.get('[data-cy="firstNameInput"]').type("firstName") 
+        cy.get('[data-cy="surnameInput"]').type("sername") 
+        cy.get('[data-cy="DOBInput"]').type("2020-01-01") 
+        cy.get('[data-cy="AddressInput"]').type("BS8 1HB") 
+        cy.get('[data-cy="PhoneNumberInput"]').type("+44 782479472") 
         cy.get('[data-cy="passwordInput"]').type("abc")
         cy.get('[data-cy="confirmPasswordInput"]').type("abc")
         cy.get('[data-cy="submitButton"]').click()
-
     })
 
 })
