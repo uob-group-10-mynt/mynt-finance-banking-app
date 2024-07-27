6432e1704d47005cba8880d9a1931db6806c4746
Jan Phillips <janpatekphillips@gmail.com>
2024-07-27 11:53:21 +0100

    [test]: test for updating user details passing consistently

--- frontend/cypress/e2e/userDetails.cy.js
+++ frontend/cypress/e2e/userDetails.cy.js
@@ -1,35 +1,25 @@
 describe('user details page', ()  => {
     it('can view and update user details', () => {
-        const firstname = 'Cypress'
-        const lastname = 'Test'
-        const dob = '1999-10-19'
-        const address = 'Bristol'
-        const phoneNumber = '07123123123'
-
-        //updated data
-        const newFirstname = 'William'
-        const newLastname = 'Shakespeare'
-        const newDob = '1999-04-23'
-        const newAddress = 'Stoke'
-        const newPhoneNumber = '07321321321'
+        let timestamp = Date.now()
+        const firstname = 'Cypress' + timestamp
+        const lastname = 'Test' + timestamp
+        const dob = new Date().toISOString().split("T")[0]
+        const address = 'Bristol' + timestamp
+        const phoneNumber = '07123123123' + timestamp
 
         cy.loginTestUser()
-        //check starting values
         cy.get('[data-cy="navButton"]').click()
         cy.get('[data-cy="DetailsLink"]').click()
-        cy.get('[data-cy="firstnameInput"]').should('have.value', firstname)
-        cy.get('[data-cy="lastnameInput"]').should('have.value', lastname)
-        cy.get('[data-cy="dobInput"]').should('have.value', dob)
-        cy.get('[data-cy="addressInput"]').should('have.value', address)
-        cy.get('[data-cy="phoneNumberInput"]').should('have.value', phoneNumber)
-        
-        //update
+
+        //set new details
         cy.get('[data-cy="EditButton"]').click()
-        cy.get('[data-cy="firstnameInput"]').clear().type(newFirstname)
-        cy.get('[data-cy="lastnameInput"]').clear().type(newLastname)
-        cy.get('[data-cy="dobInput"]').type(newDob)
-        cy.get('[data-cy="addressInput"]').clear().type(newAddress)
-        cy.get('[data-cy="phoneNumberInput"]').clear().type(newPhoneNumber)
+        cy.get('[data-cy="EditButton"]').click()
+
+        cy.get('[data-cy="firstnameInput"]').clear().type(firstname)
+        cy.get('[data-cy="lastnameInput"]').clear().type(lastname)
+        cy.get('[data-cy="dobInput"]').type(dob)
+        cy.get('[data-cy="addressInput"]').clear().type(address)
+        cy.get('[data-cy="phoneNumberInput"]').clear().type(phoneNumber)
         cy.get('[data-cy="saveDetailsButton"]').click()
 
         //navigate away and back to page
@@ -39,19 +29,10 @@ describe('user details page', ()  => {
         cy.get('[data-cy="DetailsLink"]').click()
 
         //assert changes
-        cy.get('[data-cy="firstnameInput"]').should('have.value', newFirstname).should('have.attr', 'readonly')
-        cy.get('[data-cy="lastnameInput"]').should('have.value', newLastname).should('have.attr', 'readonly')
-        cy.get('[data-cy="dobInput"]').should('have.value', newDob).should('have.attr', 'readonly')
-        cy.get('[data-cy="addressInput"]').should('have.value', newAddress).should('have.attr', 'readonly')
-        cy.get('[data-cy="phoneNumberInput"]').should('have.value', newPhoneNumber).should('have.attr', 'readonly')
-
-        //reset form
-        cy.get('[data-cy="EditButton"]').click()
-        cy.get('[data-cy="firstnameInput"]').clear().type(firstname)
-        cy.get('[data-cy="lastnameInput"]').clear().type(lastname)
-        cy.get('[data-cy="dobInput"]').type(dob)
-        cy.get('[data-cy="addressInput"]').clear().type(address)
-        cy.get('[data-cy="phoneNumberInput"]').clear().type(phoneNumber)
-        cy.get('[data-cy="saveDetailsButton"]').click()
+        cy.get('[data-cy="firstnameInput"]').should('have.value', firstname).should('have.attr', 'readonly')
+        cy.get('[data-cy="lastnameInput"]').should('have.value', lastname).should('have.attr', 'readonly')
+        cy.get('[data-cy="dobInput"]').should('have.value', dob).should('have.attr', 'readonly')
+        cy.get('[data-cy="addressInput"]').should('have.value', address).should('have.attr', 'readonly')
+        cy.get('[data-cy="phoneNumberInput"]').should('have.value', phoneNumber).should('have.attr', 'readonly')
     })
 })
