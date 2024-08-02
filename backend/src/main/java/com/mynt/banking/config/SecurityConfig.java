dc04772f2c7de8f947ba3d1f0eb84dd7474ec163
yy <uv23869@bristol.ac.uk>
2024-08-02 14:14:24 +0800

    [feat]: Add beneficiaries/add endpoint

--- backend/src/main/java/com/mynt/banking/config/SecurityConfig.java
+++ backend/src/main/java/com/mynt/banking/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
             "/api/v1/auth/**",
             "/api/v1/auth/sdk**",
+            "/api/v1/currency-cloud/beneficiaries/**",
             "/v3/api-docs/**",
             "/swagger-resources",
             "/swagger-resources/**",
