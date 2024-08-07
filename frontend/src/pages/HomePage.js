0220075d581e4e9b19e6cb427b5a5ea6744a1b98
Jan Phillips <janpatekphillips@gmail.com>
2024-08-07 07:03:55 +0100

    [feature]: homepage aesthetics might be good enough to show to the group

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,63 +4,54 @@ import {
     CardBody,
     Heading,
     Box,
-    Text
-
+    Text,
+    Container,
+    Image
 } from "@chakra-ui/react";
-//TODO sticky header
+import CustomButton from "../components/forms/CustomButton";
+import graphic from '../../public/images/mobile_banking612x612.jpg'
+import { Link } from "react-router-dom";
 
 
 export default function HomePage() {
     return(
-        <Box 
+    <Box 
         maxW={["100%", "100%", "90%", "1024px"]}
         justifyContent="center"
         alignItems="center"
         margin='auto'
 
         >
-            <Card mt='3rem'>
-                <CardHeader>
-                    <Heading as='h1' >Kenya's first international neobank</Heading>
-                </CardHeader>
-                <CardBody>
-                    <Box>
-                        <Heading size='xs' textTransform='uppercase'>
-                            Access all of our services from your device
-                        </Heading>
-                        <Text pt='2' fontSize='sm'>
-                        Open an account today
-                            (put link here)
-                        </Text>
-                    </Box>
-                </CardBody>
-            </Card>
+        <Container maxW="container.xl" py={20} textAlign="center">
+            <Heading as="h1" size="2xl" mb={4}>
+            Welcome to Kenya's first international neobank
+            </Heading>
+            <Text fontSize="xl" mb={6}>
+            Modern banking, simplified. Manage your finances with ease.
+            </Text>
+            <Link to='/signup'>
+                <CustomButton colorScheme="teal" size="lg" color='white'>Get Started</CustomButton>
+            </Link>
+            <Image src={graphic} alt="Banking Illustration" mt={10} mx="auto" />
+        </Container>
 
-            <Card mt='2rem'>
-                <CardHeader>
-                    <Heading as='h1' >Transfer domestically or internationally</Heading>
-                </CardHeader>
-                <CardBody>
-                    <Heading as='h2' size='md'>All from the same account, using IBAN (more support to come)</Heading>
-                </CardBody>
-            </Card>
+        <Card mt='2rem' textAlign="center">
+            <CardHeader>
+                <Heading as='h1' >Transfer domestically or internationally</Heading>
+            </CardHeader>
+            <CardBody>
+                <Heading as='h2' size='md'>All from the same account, using IBAN (more support to come)</Heading>
+            </CardBody>
+        </Card>
 
-            <Card mt='2rem'>
+            <Card mt='2rem' textAlign="center">
                 <CardHeader>
                     <Heading as='h1' >Choose from a vast array of currencies</Heading>
                 </CardHeader>
                 <CardBody>
-                    <Heading as='h2' size='md'>Kenya's first international neobank</Heading>
-                    <Box>
-                        <Heading size='xs' textTransform='uppercase'>
-                        Summary
-                        </Heading>
-                        <Text pt='2' fontSize='sm'>
-                        View a summary of all your clients over the last month.
-                        </Text>
-                    </Box>
+                    <Heading as='h2' size='md'>KES, NGN, CNY, GBP, USB, and many more!</Heading>
                 </CardBody>
             </Card>
-        </Box>
+    </Box>
     )
 }
