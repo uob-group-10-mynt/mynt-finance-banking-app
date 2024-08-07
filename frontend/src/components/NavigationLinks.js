9d75fb5c525f3b603bfc100162f06fef4da8f3be
Jan Phillips <janpatekphillips@gmail.com>
2024-08-07 12:06:14 +0100

    [feature] accounts, dashboard, and currencies pages now available through navbar

--- frontend/src/components/NavigationLinks.js
+++ frontend/src/components/NavigationLinks.js
@@ -19,22 +19,27 @@ export default function NavigationLinks() {
     const loggedInData = [
         {text: "Transfer", id: "RemittanceLink", onClick: async ()=>{navigate("/accounts");return false}},
         {text: "Account Details", id: "DetailsLink", onClick: async ()=>{navigate("/userDetails");return false}},
+        {text: "Dashboard", id: "DashboardLink", onClick: async ()=>{navigate("/dashboard");return false}},
+        {text: "Currencies", id: "CurrenciesLink", onClick: async ()=>{navigate("/currencies");return false}},
+        {text: "Accounts", id: "AccountsLink", onClick: async ()=>{navigate("/accounts");return false}},
         {text: "Log Out", id: "LogOutLink", onClick: async ()=>{logOut();navigate("/login");return false}}
+
     ]
     const loggedOutData = [
+        {text: "Home", onClick: async ()=>{navigate("/");return false}},
         {text: "Sign Up", id: "SignUpLink", onClick: async ()=>{navigate("/signup");return false}},
-        {text: "Log In", id: "LoginLink", onClick: async ()=>{navigate("/login");return false}},
-    ]
-    const pagesThatAreAlwaysVisible = [
-        {text: "Home", onClick: async ()=>{navigate("/");return false}}
-    ];
+        {text: "Log In", id: "LoginLink", onClick: async ()=>{navigate("/login");return false}}
 
-    let links = createLinks(pagesThatAreAlwaysVisible)
+    ]
+    // const pagesThatAreAlwaysVisible = [
+    //     {text: "Home", onClick: async ()=>{navigate("/");return false}}
+    // ];
+
+    // let links = createLinks(pagesThatAreAlwaysVisible)
     if (loggedIn) {
-        links.push(createLinks(loggedInData))
+        return createLinks(loggedInData)
     } else {
-        links.push(createLinks(loggedOutData))
+        return createLinks(loggedOutData)
     }
 
-    return links;
 }
