96c07ae5c10ad3a341f3c216864e0606fd0cb710
Jan Phillips <janpatekphillips@gmail.com>
2024-08-29 13:32:06 +0100

    [fix] /accounts is a protected route; fixed login test, and login required test; both passing now

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -10,14 +10,14 @@ describe('log in page', () => {
         cy.get('[data-cy="passwordInput"]').type(testUser.password)
         cy.get('[data-cy="submitButton"]').click()
         
-        cy.url().should('eq', 'http://localhost:9001/#/accounts')
+        cy.url().should('eq', 'http://localhost:9001/accounts')
 
         cy.get('[data-cy="navButton"]').click()
         cy.get('[data-cy="RemittanceLink"]').contains('Transfer')
 
         //log out
         cy.get('[data-cy="LogOutLink"]').click()
-        cy.url().should('eq', 'http://localhost:9001/#/login')
+        cy.url().should('eq', 'http://localhost:9001/login')
     })
 
     it('displays an error message when incorrect email is used', () => {
